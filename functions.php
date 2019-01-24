1c2b49ece674879b5c4c25bf6b3d08f63e92617e
lorcann.rauzduel@epitech.eu <lorcann.rauzduel@epitech.eu@epitech.eu>
2019-01-24 21:23:38 +0100

    Test Technique

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-
 $xml = simplexml_load_file('array.xml');
+ // Fonction pour trier un tableau multidimensionnel
 function xsort(&$nodes, $child_name, $order=SORT_ASC)
 {
     $sort_proxy = array();
@@ -11,6 +11,8 @@ function xsort(&$nodes, $child_name, $order=SORT_ASC)
 
     array_multisort($sort_proxy, $order, $nodes);
 }
+ // Localiser les données
 $sessions = $xml->xpath('/Formations/Formation/Session');
+ // Trie par ordre croissant
 xsort($sessions, 'StartDate', SORT_ASC);
 ?>
